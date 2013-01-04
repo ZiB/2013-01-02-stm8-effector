59b42b3f5ee181d1b55c8f5abc9a291f88c4f8c7
Denis Zheleznyakov <zib@ziblog.ru>
2013-01-04 13:33:54 +0700

    + скорость работы программы

--- utility/task_manager.c
+++ utility/task_manager.c
@@ -9,6 +9,8 @@
 const void (* const task_list[11])(void) = { do_work, do_jump, do_strobo, do_slide, do_bright, do_blink,
 		do_eeprom_next, do_flash_next, do_slide_target, do_switch, do_strobo_blinc };
 
+static volatile uint8_t speed_counter;
+
 //------------------------------------------------------------------------------
 void task_manager_refresh(void)
 {
@@ -16,19 +18,32 @@ void task_manager_refresh(void)
 	uint8_t * period = &program.period_work;
 	uint8_t * counter = &program.counter_work;
 
-	for (task_number = 0; task_number < ARRAY_LENGHT(task_list); task_number++)
+	if (program.speed != 0)
 	{
-		// проверка активности задачи
-		if (period[task_number] != 0)
+		if (program.speed > 9)
 		{
-			// проверка готовности выполенения
-			if (++counter[task_number] >= period[task_number])
-			{
-				// сброс счетчика
-				counter[task_number] = 0;
+			program.speed = 9;
+		}
 
-				// вызов задачи
-				(*task_list[task_number])();
+		if (++speed_counter > (9 - program.speed))
+		{
+			speed_counter = 0;
+
+			for (task_number = 0; task_number < ARRAY_LENGHT(task_list); task_number++)
+			{
+				// проверка активности задачи
+				if (period[task_number] != 0)
+				{
+					// проверка готовности выполенения
+					if (++counter[task_number] >= period[task_number])
+					{
+						// сброс счетчика
+						counter[task_number] = 0;
+
+						// вызов задачи
+						(*task_list[task_number])();
+					}
+				}
 			}
 		}
 	}
