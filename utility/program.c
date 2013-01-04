373804ba023d12683fa1e34834d1336ef3c3e9c3
Denis Zheleznyakov <zib@ziblog.ru>
2013-01-04 13:40:01 +0700

    + подпрограммы эффектов

--- utility/program.c
+++ utility/program.c
@@ -21,34 +21,188 @@ const uint8_t flash_program[][PROGRAM_STORE_SIZE] = { /*волна*/{ 0, 0, 1, 4
 		3, 0, 0 }, { 0, 1, 2, 4, 8, 16, 32, 64, 128, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
 		255, 1, 0, 0, 0, 0, 0, 0 } };
 
+volatile uint8_t slide_current;
+volatile uint8_t slide_target;
+volatile uint8_t slide_restore;
+volatile uint8_t pwm_target;
+
 //------------------------------------------------------------------------------
 void do_work(void)
 {
+	uint8_t channel_number;
+	uint8_t channel_bright;
+
+	for (channel_number = 0; channel_number < ARRAY_LENGHT(program.channel_bright); channel_number++)
+	{
+		channel_bright = program.channel_bright[channel_number];
+
+		if (program.channel_effect[channel_number] > 128)
+		{
+			// увеличение яркости
+			if (channel_bright != 0)
+			{
+				channel_bright <<= 1;
+
+				if (channel_bright == 0)
+				{
+					channel_bright = 128;
+
+					program.channel_effect[channel_number] = 0;
+				}
+			}
+			else
+			{
+				channel_bright = 1;
+			}
+		}
+
+		if (program.channel_effect[channel_number] < 128)
+		{
+			// уменьшение яркости
+			if (channel_bright != 0)
+			{
+				channel_bright >>= 1;
+			}
+			else
+			{
+				program.channel_effect[channel_number] = 255;
+			}
+		}
+
+		program.channel_bright[channel_number] = channel_bright;
+	}
 }
 
 //------------------------------------------------------------------------------
 void do_jump(void)
 {
+	uint8_t channel_number;
+
+	for (channel_number = 0; channel_number < ARRAY_LENGHT(program.channel_bright); channel_number++)
+	{
+		if (program.channel_effect[channel_number] != 128)
+		{
+			if (program.channel_bright[channel_number] == 0)
+			{
+				program.channel_bright[channel_number] = 128;
+			}
+			else
+			{
+				// уменьшаем яркость
+				program.channel_bright[channel_number] >>= 1;
+			}
+		}
+	}
 }
 
 //------------------------------------------------------------------------------
 void do_strobo(void)
 {
+	uint8_t tmp;
+
+	program.strobo_out = 8;
+
+	tmp = get_random_byte();
+	tmp &= 127;
+	tmp |= 3;
+
+	program.period_strobo = tmp;
 }
 
 //------------------------------------------------------------------------------
 void do_slide(void)
 {
+	uint16_t tmp;
+
+	if (slide_current == 128)
+		return;
+
+	if (slide_current > 127)
+	{
+		tmp = 128;
+		tmp -= slide_current;
+		tmp += 40;
+		tmp += slide_restore;
+		slide_restore = (uint8_t) tmp;
+		if (tmp > 256)
+		{
+			if (program.slide > 0)
+			{
+				program.slide--;
+			}
+			else
+			{
+				program.slide = ARRAY_LENGHT(program.channel_bright) - 1;
+			}
+		}
+	}
+	else
+	{
+		tmp = slide_current;
+		tmp -= 128;
+		tmp += 40;
+		tmp += slide_restore;
+		slide_restore = (uint8_t) tmp;
+		if (tmp > 256)
+		{
+			if (program.slide < (ARRAY_LENGHT(program.channel_bright) - 1))
+			{
+				program.slide++;
+			}
+			else
+			{
+				program.slide = 0;
+			}
+		}
+	}
 }
 
 //------------------------------------------------------------------------------
 void do_bright(void)
 {
+	if (program.pwm_max == pwm_target)
+	{
+		pwm_target -= get_random_byte();
+		pwm_target |= 3;
+	}
+	if (program.pwm_max > pwm_target)
+	{
+		program.pwm_max--;
+	}
+	else
+	{
+		program.pwm_max++;
+	}
 }
 
 //------------------------------------------------------------------------------
 void do_blink(void)
 {
+	uint8_t channel_number;
+	uint8_t channel_bright;
+
+	for (channel_number = 0; channel_number < ARRAY_LENGHT(program.channel_bright); channel_number++)
+	{
+		channel_bright = program.channel_bright[channel_number];
+
+		if (get_random_byte() < 128)
+		{
+			channel_bright >>= 1;
+		}
+		else
+		{
+			if (channel_bright == 0)
+			{
+				channel_bright = 128;
+			}
+			else
+			{
+				channel_bright <<= 1;
+			}
+		}
+
+		program.channel_bright[channel_number] = channel_bright;
+	}
 }
 
 //------------------------------------------------------------------------------
@@ -64,6 +218,19 @@ void do_flash_next(void)
 //------------------------------------------------------------------------------
 void do_slide_target(void)
 {
+	if (slide_current == slide_target)
+	{
+		slide_target -= get_random_byte();
+	}
+
+	if (slide_current > slide_target)
+	{
+		slide_current--;
+	}
+	if (slide_current < slide_target)
+	{
+		slide_current++;
+	}
 }
 
 //------------------------------------------------------------------------------
@@ -74,6 +241,10 @@ void do_switch(void)
 //------------------------------------------------------------------------------
 void do_strobo_blinc(void)
 {
+	if (program.strobo_out != 0)
+	{
+		program.strobo_out--;
+	}
 }
 
 //------------------------------------------------------------------------------
@@ -90,4 +261,6 @@ void program_init(void)
 	program.period_slide_target = 20;
 	program.period_switch = 50;
 	program.pwm_max = 128;
+
+	slide_current = 128;
 }
